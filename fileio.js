59e0317e34b75ef136170c09716205186db838b6
Mark Hanna <cipscis+github@gmail.com>
2021-08-14 21:54:07 +1200

    Converted to TypeScript

--- fileio.js
+++
@@ -1,2 +0,0 @@
-export { load } from './fileio/load.js';
-export { save } from './fileio/save.js';
---
+++ src/fileio.ts
@@ -0,0 +1,4 @@
+export { load } from './load.js';
+export { save, SaveOptions } from './save.js';
+
+export { ReadMethod } from './readmethod.js';
